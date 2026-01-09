71c35f05f9e2624c8a560678fb2386b7b67f4ac7
KingRishad <100399479+KingRishad@users.noreply.github.com>
2026-01-09 17:59:27 +0600

    Update Potion.java

--- Lab 4/Problem 1/Potion.java
+++ Lab 4/Problem 1/Potion.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Potion;
 
 /**
@@ -34,3 +30,4 @@ public class Potion {
         this.effect = effect;
     }
 }
+
