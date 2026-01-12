16f5841a89f092417beaa1b3208e9cb757f74cd8
KingRishad <100399479+KingRishad@users.noreply.github.com>
2026-01-13 00:33:55 +0600

    Update StudentManager.java

--- Lab 5/Problem 1/StudentManager.java
+++ Lab 5/Problem 1/StudentManager.java
@@ -5,12 +5,25 @@ package studentmanagement;
  * @author The one who shall not be named
  */
 public class StudentManager {
-    public static Student searchStudentById(Student[] students, int id) {
+    public class StudentManager {
+        public static Student searchStudentById(Student[] students, int id) {
+            for (int i = 0; i < students.length; i++) {
+                if (students[i].getId() == id) {
+                    return students[i];
+                }
+            }
+            return null;
+        }
+
+    
+    
+    /*public static Student searchStudentById(Student[] students, int id) {
         for (Student s : students) {
             if (s.getId() == id) {
                 return s;
             }
         }
         return null;
-    }
+    }*/
 }
+
