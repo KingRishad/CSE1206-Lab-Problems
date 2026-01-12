1f54fd2561736e4bde2420d269df43d6db5fbc64
KingRishad <100399479+KingRishad@users.noreply.github.com>
2026-01-13 00:37:51 +0600

    Refactor StudentManager class and clean up code

--- Lab 5/Problem 1/StudentManager.java
+++ Lab 5/Problem 1/StudentManager.java
@@ -5,15 +5,14 @@ package studentmanagement;
  * @author The one who shall not be named
  */
 public class StudentManager {
-    public class StudentManager {
-        public static Student searchStudentById(Student[] students, int id) {
-            for (int i = 0; i < students.length; i++) {
-                if (students[i].getId() == id) {
-                    return students[i];
-                }
-            }
-            return null;
+    public static Student searchStudentById(Student[] students, int id) {
+        for (int i = 0; i < students.length; i++) {
+            if (students[i].getId() == id) {
+                  return students[i];
+             }
         }
+         return null;
+     }
 
     
     
@@ -29,5 +28,6 @@ public class StudentManager {
         return null;
     }*/
 }
+
 
 
